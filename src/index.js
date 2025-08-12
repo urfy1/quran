e58410e72c82488488692020f1ae753c004cfd91
urfy1 <urfan.azim@gmail.com>
2025-08-12 11:35:24 +0100

    Your descriptive commit message here

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'; // <-- The import is already here
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -15,10 +15,9 @@ root.render(
   </StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
+// This line is the key change!
+// Change unregister() to register() to enable PWA features.
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
